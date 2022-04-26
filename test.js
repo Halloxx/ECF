2b20fb204fa105c928e5c04d35f4f258f669686f
Halloxx <axeltaek37@gmail.com>
2022-04-26 14:12:35 +0200

    add website files

---
+++ test.js
@@ -0,0 +1,54 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName('slides');
+    let dots = document.getElementsByClassName('dot');
+    
+    if(n > slides.length) { slideIndex = 1 }
+    
+    if(n < 1 ) { slideIndex = slides.length }
+    
+    // Cacher toutes les slides
+    for(let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    
+    // Retirer "active" de tous les points
+    for(let i = 0; i < dots.length; i++) {
+      dots[i].classList.remove('active');
+    }
+    
+    // Afficher la slide demandée
+    slides[slideIndex - 1].style.display = 'block';
+    
+    // Ajouter "active" sur le point cliqué
+    dots[slideIndex - 1].classList.add('active');
+  }
+
+
+
+
+
+  module.export = {
+
+    development: {
+      client: 'mysql2' ,
+      connection: () => ({
+        host : process.env.DB_HOST,
+        user : process.env.DB_USER,
+        password : process.env.DB_USER,
+
+      })
+      debug: true
+    }
+  }
+  
